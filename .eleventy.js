c265eba1801e2e2e6ddbf985a93794f2ba2cf02b
caterpilllar <123208987+caterpilllar@users.noreply.github.com>
2024-03-04 16:05:55 -0500

    h1 fixed

--- .eleventy.js
+++ .eleventy.js
@@ -3,3 +3,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
+
+function nameGenerator(){
+  const names = ['bob', 'sue', 'joanne', 'nate', 'beth', 'andy', 'john', 'lilly', 'james', 'carl'];
+
+  return{
+    get name(){
+      names[Math.floor(Math.random() * names.length)]
+    }
+  }
+}
